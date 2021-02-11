8eb2916ad05be8b30bcda8b93fb636d623124422
Alex <sizykh.ad@phystech.edu>
2021-02-11 06:10:12 +0300

    Progress bar added

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"bufio"
 	"encoding/json"
 	"flag"
 	"fmt"
@@ -10,10 +11,11 @@ import (
 	"os"
 	"path/filepath"
 	"strings"
+	"sync"
 	"time"
 
-	"github.com/cheggaaa/pb/v3"
 	"github.com/jlaffaye/ftp"
+	"github.com/sethgrid/multibar"
 )
 
 type BasePathsResponse struct {
@@ -21,22 +23,71 @@ type BasePathsResponse struct {
 	Fastq_ftp     string
 }
 
+type PassThru struct {
+	io.Reader
+	progressBar *multibar.ProgressFunc
+	Start       time.Time
+	total       int // Total # of bytes transferred
+
+}
+
+// Read 'overrides' the underlying io.Reader's Read method.
+// This is the one that will be called by io.Copy(). We simply
+// use it to keep track of byte counts and then forward the call.
+func (pt *PassThru) Read(p []byte) (int, error) {
+	n, err := pt.Reader.Read(p)
+	pt.total += int(n)
+	(*pt.progressBar)(pt.total)
+	return n, err
+}
+
 func main() {
-	srrPtr := flag.String("i", "", "SRA accession (Required)")
+	srrPtr := flag.String("i", "", "SRA accession or path to file with SRA accessions(requires -L flag)(Required)")
 	outDirPtr := flag.String("O", "", "Output directory")
+
+	listFlag := flag.Bool("L", false, "Input is a file with SRA accessions")
 	flag.Parse()
 
 	if *srrPtr == "" {
 		flag.PrintDefaults()
 		os.Exit(1)
 	}
-	directDownloadPaths := GetFtpLinks(*srrPtr)
-	//fmt.Println(directDownloadPaths)
-	for _, fileName := range directDownloadPaths {
-		log.Printf("Start download:%v\r\n", fileName)
-		DownloadFtpFile(fileName, *outDirPtr)
+	var directDownloadPaths []string
+	if *listFlag == false {
+		directDownloadPaths = GetFtpLinks(*srrPtr)
+	} else {
+		file, err := os.Open(*srrPtr)
+		if err != nil {
+			fmt.Printf("Couldn't open accession file: \n%s \nfor reading, exiting\n", *srrPtr)
+			os.Exit(1)
+
+		}
+		var accessions []string
+		defer file.Close()
+		scanner := bufio.NewScanner(file)
+		for scanner.Scan() {
+			accessions = append(accessions, scanner.Text())
+		}
+		for _, accession := range accessions {
+			for _, tmp := range GetFtpLinks(accession) {
+				directDownloadPaths = append(directDownloadPaths, tmp)
+
+			}
+		}
 
 	}
+	//fmt.Println(directDownloadPaths)
+	var wg sync.WaitGroup
+	numBars := len(directDownloadPaths)
+	progressBars, _ := multibar.New()
+	wg.Add(numBars)
+	for _, fileName := range directDownloadPaths {
+
+		go DownloadFtpFile(fileName, *outDirPtr, &wg, progressBars)
+
+	}
+	go progressBars.Listen()
+	wg.Wait()
 }
 
 func GetFtpLinks(srr string) []string {
@@ -45,20 +96,23 @@ func GetFtpLinks(srr string) []string {
 	//fmt.Println(url)
 	res, err := myClient.Get(url)
 	if err != nil {
-		panic(err.Error())
+		fmt.Printf("Accession %s not found, exiting.", srr)
+		os.Exit(1)
 	}
 	defer res.Body.Close()
 
 	var response []BasePathsResponse
 	if err = json.NewDecoder(res.Body).Decode(&response); err != nil {
-		panic(err.Error())
+		fmt.Printf("Accession %s not available, exiting.\n", srr)
+		os.Exit(1)
 	}
 	paths := strings.Split(response[0].Fastq_ftp, ";")
 	return paths
 
 }
 
-func DownloadFtpFile(remoteFile string, outPath string) {
+func DownloadFtpFile(remoteFile string, outPath string, wg *sync.WaitGroup, progressBars *multibar.BarContainer) {
+	defer wg.Done()
 
 	startTime := time.Now()
 
@@ -85,17 +139,17 @@ func DownloadFtpFile(remoteFile string, outPath string) {
 		fmt.Println("Error estimating file size.\nExiting.")
 		os.Exit(1)
 	}
+	name := fmt.Sprintf("%s:", filepath.Base(localFileName))
+
 	log.Printf("File size: %v bytes", limit)
 	r, err := conn.Retr(fileName)
 	if err != nil {
 		panic(err)
 	}
 	defer r.Close()
-
-	bar := pb.Full.Start64(limit)
-	defer bar.Finish()
-	barReader := bar.NewProxyReader(r)
-	_, err = io.Copy(localFile, barReader)
+	barProgress := progressBars.MakeBar(int(limit), name)
+	response := &PassThru{Reader: r, progressBar: &barProgress, Start: startTime}
+	_, err = io.Copy(localFile, response)
 	if err != nil {
 		panic(err)
 	}
